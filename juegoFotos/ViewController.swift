f2d286d3b69aa61d9c4f87b7a7083e2e10866107
ricardo <ricardo_morales_apps1ma1819@cev.com>
2019-10-17 11:46:20 +0200

    Add new images and show the images random in the first view

--- juegoFotos/ViewController.swift
+++ juegoFotos/ViewController.swift
@@ -3,10 +3,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ImageCenter: UIImageView!
+    var images: [UIImage] = [#imageLiteral(resourceName: "cyndaquil"), #imageLiteral(resourceName: "metapod"), #imageLiteral(resourceName: "diglet"), #imageLiteral(resourceName: "zubat"), #imageLiteral(resourceName: "pikachu"), #imageLiteral(resourceName: "magikarp"), #imageLiteral(resourceName: "pidgey"), #imageLiteral(resourceName: "zapdos"), #imageLiteral(resourceName: "rattata")]
+    
+    var ordenAleatorio: [Int] = []
 
+    @IBOutlet weak var play: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        Imgrandom()
+        show()
         
+    }
+    
+    func Imgrandom(){
+        for i in 1...8 {
+            ordenAleatorio.append(i)
+        }
+         ordenAleatorio.shuffle()
+    }
+    
+    func show(){
+        var i = 0
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
+            if i < 3 {
+                self.ImageCenter.image = self.images[self.ordenAleatorio[i]]
+            }
+            i+=1
+            if i == 3 {
+                self.play.isHidden = false
+            }
+        })
     }
 
 
